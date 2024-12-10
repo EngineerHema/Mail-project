afefb8d44eb5a09dc3aa9daf9c81672895fc7113
John William <johnwilliamfawzy@gmail.com>
2024-12-10 02:02:11 +0100

    Scrollable container and base cards added

--- front-end/src/inbox.js
+++ front-end/src/inbox.js
@@ -1,10 +1,17 @@
 import SideBar from "./Bars/SideBar";
 import NavBar from "./Bars/NavBar";
+import ScrollableContainer from './ScrollableContainer';
+import './Scrollablecontainer.css'; 
+
 function Inbox() {
     return (
      <div>
+      
         <NavBar></NavBar>
+        <div className="scrollable_flex">
         <SideBar></SideBar>
+        <ScrollableContainer/>
+      </div>
      </div>
     );
   }
